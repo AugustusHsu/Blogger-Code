c0dc25681b427d59706d8f22b76601d73321d89b
AugustusHsu <jimhsu11@gmail.com>
2020-09-12 03:06:08 +0800

    Update utils.py
    Add some function to the utils.py, get new figure, and fix some problem

--- GAN-01 Generative Adversarial Network/utils.py
+++ GAN-01 Generative Adversarial Network/utils.py
@@ -2,18 +2,20 @@
 """
 Created on Wed May 13 16:08:31 2020
 
-@author: jimhs
+@author: jim
 """
 
 import os
 import imageio
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from args import parser
 
 opts = parser()
 
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+cross_entropy = tf.keras.losses.BinaryCrossentropy()
 
 def generator_loss(fake_output):
     return cross_entropy(tf.ones_like(fake_output), fake_output)
@@ -34,10 +36,11 @@ def generate_and_save_images(Generator, epoch, test_input):
     predictions = Generator(test_input, training=False)
     
     fig = plt.figure(figsize=(16,16))
-    
+    image = np.uint8(predictions*127.5 + 127.5)
+    image = np.squeeze(image)
     for i in range(predictions.shape[0]):
         plt.subplot(4, 4, i+1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.imshow(image[i], cmap='gray')
         plt.axis('off')
     fig.suptitle('{}'.format(epoch), fontsize=70)
     path = os.path.join(fig_path, '{:03d}.png'.format(epoch))
@@ -52,7 +55,19 @@ def plot_GIF(anim_file = 'gan.gif'):
         images.append(img)
     imageio.mimsave(anim_file, images)
 
-
+def plot_line(df, col_name, method, figname):
+    # Initial Log File
+    fig_path = os.path.join(opts.PLOT_LINE_PATH)
+    if not os.path.exists(fig_path):
+        os.mkdir(fig_path)
+        
+    fig, ax = plt.subplots(figsize=(16,10))
+    sns.lineplot(data=df[col_name])
+    
+    fig.suptitle(figname, fontsize=50)
+    path = os.path.join(fig_path, method + '_' + figname + '.png')
+    plt.savefig(path)
+    plt.close('all')
 
 
 
